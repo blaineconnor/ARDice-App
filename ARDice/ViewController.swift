a17e540e7c11326033bcf21fa48da9b31627b7a7
blaineconnor <fesarman@anadolu.edu.tr>
2024-07-09 13:33:04 +0300

    Project Completed

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -10,23 +10,45 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
+    var diceArray = [SCNNode()]
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        //        let sphere = SCNSphere(radius: 0.2)
+        //
+        //        let material = SCNMaterial()
+        //
+        //        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+        //
+        //        sphere.materials = [material]
+        //
+        //        let node = SCNNode()
+        //
+        //        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        //        node.geometry = sphere
+        //
+        //        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        //        // Create a new scene
+        //        let diceScene = SCNScene(named: "art.scnassets/A-6.scn")!
+        //
+        //        if let diceNode = diceScene.rootNode.childNode(withName: "A-6", recursively: true) {
+        //
+        //            diceNode.position = SCNVector3(0, 0, -0.1)
+        //
+        //            sceneView.scene.rootNode.addChildNode(diceNode)
+        //        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,7 +56,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        configuration.planeDetection = .horizontal
+        
+        print("Session is supported = \(ARConfiguration.isSupported)")
+        print("World Tracking is supported = \(ARWorldTrackingConfiguration.isSupported)")
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -45,17 +72,94 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    diceNode.position = SCNVector3(
+                        x: hitResult.worldTransform.columns.3.x,
+                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                        z: hitResult.worldTransform.columns.3.z)
+                    
+                    diceArray.append(diceNode)
+                    
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                    roll(dice: diceNode)
+                }
+            }
+        }
     }
-*/
+    
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode) {
+        
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        dice.runAction(
+            SCNAction.rotateBy(
+                x: CGFloat(randomX * 5),
+                y: 0,
+                z: CGFloat(randomZ * 5),
+                duration: 0.5)
+        )
+    }
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                dice.removeFromParentNode()
+            }
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor){
+        if anchor is ARPlaneAnchor {
+            let planeAncher = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAncher.extent.x), height: CGFloat(planeAncher.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAncher.center.x, y: planeAncher.center.y, z: planeAncher.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
+        } else {
+            return
+        }
+    }
+    
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
